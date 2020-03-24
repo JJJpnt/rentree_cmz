97fcf453af8431259dc558d1ac8cefad9a6c8be9
Jean-Julien Jupinet <jean.julien.jupinet@gmail.com>
2020-03-24 16:33:45 +0100

    gg jj

--- include/nav.php
+++ include/nav.php
@@ -1,10 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-blue-dark">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
+<nav class="navbar navbar-expand-lg navbar-dark bg-blue-dark justify-content-around">
+  <a class="navbar-brand order-2" href="#">Rentrée CmZ</a>
+  <div class="collapse navbar-collapse order-1" id="navbarText">
+    <ul class="navbar-nav mx-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
@@ -15,8 +12,21 @@
         <a class="nav-link" href="#">Pricing</a>
       </li>
     </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
   </div>
+  <div class="collapse navbar-collapse order-3" id="navbarText">
+    <ul class="navbar-nav order-3 mx-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 </nav>
