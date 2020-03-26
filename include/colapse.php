1e8db5db5b964b2acfc053759d4b59068b0105ba
Anthony08240 <beenen@simplon-charleville.fr>
2020-03-26 16:38:25 +0100

    1.4

---
+++ include/colapse.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Untitled</title>
+  <link rel="stylesheet" href="colapse/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700">
+  <link rel="stylesheet" href="colapse/css/Accordion.css">
+  <link rel="stylesheet" href="colapse/css/menu-collapse-ultimate.css">
+  <link rel="stylesheet" href="colapse/css/styles.css">
+</head>
+
+<body>
+  <div class="accordion_bk">
+    <div class="container">
+
+
+      <div class="row mb-5 mt-5">
+        <div class="col-lg-12 mx-auto">
+          <!-- Accordion -->
+          <div id="accordionExample" class="accordion shadow">
+
+            <!-- Accordion item 1 -->
+            <div class="card">
+              <div id="headingOne" class="card-header bg-white shadow-sm border-0">
+                <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne"
+                    class="d-block position-relative text-dark text-uppercase collapsible-link py-2">FAQ 1</a></h6>
+              </div>
+              <div id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample" class="collapse show">
+                <div class="card-body p-5">
+                  <p class="font-weight-light m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+                    pariatur fugit necessitatibus voluptatem, eos architecto, sunt aliquam, quae reiciendis qui
+                    accusantium corporis. Iure sequi soluta ullam fuga adipisci eos tempora!</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion item 2 -->
+            <div class="card">
+              <div id="headingTwo" class="card-header bg-white shadow-sm border-0">
+                <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="false" aria-controls="collapseTwo"
+                    class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">FAQ 2</a>
+                </h6>
+              </div>
+              <div id="collapseTwo" aria-labelledby="headingTwo" data-parent="#accordionExample" class="collapse">
+                <div class="card-body p-5">
+                  <p class="font-weight-light m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+                    adipisci, aspernatur ducimus esse eaque officiis necessitatibus debitis odio ipsam praesentium
+                    labore magnam amet inventore tenetur dicta corporis. Reiciendis, veniam odio?</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Accordion item 3 -->
+            <div class="card">
+              <div id="headingThree" class="card-header bg-white shadow-sm border-0">
+                <h6 class="mb-0 font-weight-bold"><a href="#" data-toggle="collapse" data-target="#collapseThree"
+                    aria-expanded="false" aria-controls="collapseThree"
+                    class="d-block position-relative collapsed text-dark text-uppercase collapsible-link py-2">FAQ 3</a>
+                </h6>
+              </div>
+              <div id="collapseThree" aria-labelledby="headingThree" data-parent="#accordionExample" class="collapse">
+                <div class="card-body p-5">
+                  <p class="font-weight-light m-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+                    assumenda asperiores, dignissimos expedita necessitatibus vitae nesciunt aut neque, iusto autem,
+                    fugit dolorem officiis repellat eligendi ad facilis soluta omnis at.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="colapse/js/jquery.min.js"></script>
+  <script src="colapse/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
