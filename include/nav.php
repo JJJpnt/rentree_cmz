d49f416caaaacf3f371a244630c9e0fb8f414436
Jean-Julien Jupinet <jean.julien.jupinet@gmail.com>
2020-03-26 15:09:01 +0100

    Navbar

--- include/nav.php
+++ include/nav.php
@@ -1,59 +1,61 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-blue-dark justify-content-between sticky-top">
-    <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse order-4 order-lg-1 justify-content-center" id="navbarToggler">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="collapse navbar-collapse order-5 order-lg-2 justify-content-center" id="navbarToggler">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            RDE
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Activité A</a>
-            <a class="dropdown-item" href="#">Activité B</a>
-            <a class="dropdown-item" href="#">Activité C</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-
-    <div class="navbar-brand mx-auto order-2 order-lg-3 justify-content-center"><img class="logocmz-nav img-fluid" src="img/logocmz.svg"></div>
-
-    <div class="collapse navbar-collapse order-6 order-lg-4 justify-content-center" id="navbarToggler">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Editions
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">2020</a>
-            <a class="dropdown-item" href="#">1979</a>
-            <a class="dropdown-item" href="#">1515</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-
-    <div class="collapse navbar-collapse order-7 order-lg-5 justify-content-center" id="navbarToggler">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Contact
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">FAQ</a>
-            <a class="dropdown-item" href="#">Formulaire</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-</nav>
+<div class="container-fluid navbar-container fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark justify-content-between">
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse order-4 order-lg-1 justify-content-center" id="navbarToggler">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link policetitres" href="#">Accueil<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+  
+      <div class="collapse navbar-collapse order-5 order-lg-2 justify-content-center" id="navbarToggler">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown policetitres">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              RDE
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Activité A</a>
+              <a class="dropdown-item" href="#">Activité B</a>
+              <a class="dropdown-item" href="#">Activité C</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+  
+      <div class="navbar-brand mx-auto order-2 order-lg-3 justify-content-center"><img class="logocmz-nav img-fluid" src="img/logocmz.svg"></div>
+  
+      <div class="collapse navbar-collapse order-6 order-lg-4 justify-content-center" id="navbarToggler">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown policetitres">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Editions
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">2020</a>
+              <a class="dropdown-item" href="#">1979</a>
+              <a class="dropdown-item" href="#">1515</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+  
+      <div class="collapse navbar-collapse order-7 order-lg-5 justify-content-center" id="navbarToggler">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown policetitres">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Contact
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">FAQ</a>
+              <a class="dropdown-item" href="#">Formulaire</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+  </nav>
+</div>
