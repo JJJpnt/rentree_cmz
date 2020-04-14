a57d4f13a93e903b86b4d04982030bcbfdc3650c
valentincodage08 <valentin_mg08@hotmail.fr>
2020-04-14 16:45:06 +0200

    avancement index testimonials, partenaires, resize

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,12 @@
 <!-- Footer 1 -->
-<div class="row bg-yellow-light height-mid">
-    <div class="col-12 col-blue-dark my-auto text-center">Footer 1</div>
-</div>
+<div class="container-fluid">
+    <div class="row bg-yellow-light height-mid">
+        <div class="col-12 col-blue-dark my-auto text-center">Footer 1</div>
+    </div>
 
-<!-- Footer 2 -->
-<div class="row bg-azur-light height-small">
-    <div class="col-12 col-blue-dark my-auto text-center">Footer 2</div>
+    <!-- Footer 2 -->
+    <div class="row bg-azur-light height-small">
+        <div class="col-12 col-blue-dark my-auto text-center">Footer 2</div>
+    </div>
 </div>
 
