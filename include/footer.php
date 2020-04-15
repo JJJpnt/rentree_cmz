7c297b55798692aef87e12563a2f02f13fac8ddf
Jean-Julien Jupinet <jean.julien.jupinet@gmail.com>
2020-04-15 15:32:36 +0200

    footer mod jjj

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
-<footer>
+<footer class="container-fluid">
        
-    <div class="col-footer d-md-flex  ">
-        <div class="sous-footer col-md-4">
+    <div class="col-footer d-lg-flex  ">
+        <div class="sous-footer col-lg-3">
             <p>
                 <h3 class="title-footer">Liens</h3>
                 <a class="liens-footer" href="index.php">Accueuil</a> <br>
@@ -11,11 +11,11 @@
             </p>
         </div>
 
-        <div class="sous-footer col-md-4">
+        <div class="sous-footer col-lg-6">
 
             <p>
                 <h3 class="title-footer">Où nous trouver</h3>
-                <a class="liens-footer" href=""> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.671442403092!2d4.71720341559924!3d49.773437043794644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e0ea245dbdd%3A0xabe3ef5062a04d69!2sMairie%20de%20Charleville-M%C3%A9zi%C3%A8res!5e0!3m2!1sfr!2sfr!4v1586952005291!5m2!1sfr!2sfr" width="280" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></a> <br>
+                <a class="liens-footer" href=""> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2576.671442403092!2d4.71720341559924!3d49.773437043794644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ea0e0ea245dbdd%3A0xabe3ef5062a04d69!2sMairie%20de%20Charleville-M%C3%A9zi%C3%A8res!5e0!3m2!1sfr!2sfr!4v1586952005291!5m2!1sfr!2sfr" width="90%" height="220" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></a> <br>
                 
 
 
@@ -24,7 +24,7 @@
 
         </div>
 
-        <div class="sous-footer col-md-4">
+        <div class="sous-footer col-lg-3">
 
             <p>
                 <h3 class="title-footer">A savoir</h3>
