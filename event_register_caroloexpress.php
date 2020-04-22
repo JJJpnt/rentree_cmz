3cb1f4d5849eee424029b06c84ef9703ef2af3e2
Jean-Julien Jupinet <jean.julien.jupinet@gmail.com>
2020-04-22 14:21:26 +0200

    formulaire fini

--- event_register_caroloexpress.php
+++ event_register_caroloexpress.php
@@ -7,9 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  <link rel="stylesheet" href="assets/css/style.css">
 
   <title>Inscription Carolo Express</title>
 </head>
@@ -44,7 +42,7 @@
 
   <!-- Formumaire -->
 
-  <section class="get-in-touch">
+  <section class="get-in-touch container-lg">
     <center>
       <h1 class="title">Inscription Carolo Express</h1><br><br>
       <p class="text_text policep">Merci de remplir les champs ci-dessous pour inscrire votre équipe
@@ -57,7 +55,7 @@
     <h2 class="title2">Etablissement</h2>
     <form class="contact-form row">
       <div class="form-field col-lg-12">
-        <center><select name="etablissement" id="etablissement">
+        <center><select class="custom-select" name="etablissement" id="etablissement">
             <option value="IUTRCC">Institut Universitaire de Technologie (IUT RCC)</option>
             <option value="EiSINe">Ecole d’Ingénieurs en Sciences Industrielles et Numérique (EiSINe)</option>
             <option value="INSPE">Institut national Supérieur du Professorat et de l’Education (INSPÉ)</option>
@@ -265,9 +263,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
     integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
   </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-  </script>
+  <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
 </body>
 
 </html>
