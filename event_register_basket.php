6db41520a56c37f922b2c50abcff7a90162c72c0
valentincodage08 <valentin_mg08@hotmail.fr>
2020-05-15 15:52:51 +0200

    yftifdjvqsdufnqdchioqskdcvjuydcbuyqdsicjusqgj

--- event_register_basket.php
+++ event_register_basket.php
@@ -55,7 +55,7 @@ Le tournoi de basket aura lieu le vendredi 25 septembre à partir de 17h à l’
     <h2 class="title2 mt-3">Etablissement</h2>
     <form class="contact-form row w-100 justify-content-center">
       <div class="form-field col-11 col-sm-11 col-md-10 col-lg-8 col-xl-7">
-        <center><select class="custom-select" name="etablissement" id="etablissement">
+        <center><select class="custom-select" name="etablissement" id="etablissement-font-2">
             <option value="IUTRCC">Institut Universitaire de Technologie (IUT RCC)</option>
             <option value="EiSINe">Ecole d’Ingénieurs en Sciences Industrielles et Numérique (EiSINe)</option>
             <option value="INSPE">Institut national Supérieur du Professorat et de l’Education (INSPÉ)</option>
