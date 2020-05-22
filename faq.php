8671b5dc7eff1cad1c105a684b81101c51a655c1
valentincodage08 <valentin_mg08@hotmail.fr>
2020-05-22 16:50:26 +0200

    Correction de contenu

--- faq.php
+++ faq.php
@@ -45,10 +45,7 @@
     <div class="txt_faq col-xl-6 col-lg-12 col-md-12 d-flex flex-column align-items-center"><br>
       <h2 class="policetitres-proginc" style="color: var(--color-blue-middle);">S'Inscrire</h2>
       <div class="w-75 mt-3">
-      <p class="faqpolicep mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quae nihil quibusdam nobis quo sunt eligendi
-        rem, nulla fugit odio dolorem non explicabo a mollitia obcaecati, accusamus quam recusandae consequatur.<br>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quae nihil quibusdam nobis quo sunt eligendi
-        rem, nulla fugit odio dolorem non explicabo a mollitia obcaecati, accusamus quam recusandae consequatur.</p>
+      <p class="faqpolicep mb-3">Cliquez ci-dessous pour accéder à l'inscription aux évènements.</p>
 </div>
       <a href="event.php"><button class="btn btn-secondary"><h2 class="policetitres-proginc" style="color: var(--color-yellow-middle); margin-bottom: 0!important;">S'inscrire</h2></button></a>
     </div>
@@ -56,7 +53,7 @@
   <!-- footer -->
   <div class="mb-2"></div>
 
-  <?php include("include/footer.php"); ?>
+  <?php //include("include/footer.php"); ?>
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
